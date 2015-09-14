200f6f8ec86580a92075efa5ad1d53a747cf12f0
Sérgio Giraldo <sergiorgiraldo@gmail.com>
2015-09-14 15:51:24 -0300

    command to update java file

--- do.sh
+++ do.sh
@@ -6,7 +6,12 @@ bash ~/start-hbase.sh
 echo "clone git"
 git clone https://github.com/xldrx/cloudapp-mp4.git
 
+echo "change directory"
 cd cloudapp-mp4/
+
+echo "get file"
+rm -rf SuperTable.java
+wget https://raw.githubusercontent.com/sergiorgiraldo/cloudapp-mp4/master/SuperTable.java
 
 echo "submit"
 bash submit.sh
